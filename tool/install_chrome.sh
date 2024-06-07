b9379a1baf7c748a466839bc4f10205f369a3132
JohnDoe <John.Doe4development.posethoye@gmail.com>
2024-06-07 19:34:41 +0900

    feat: chromeインストールスクリプトの追加

---
+++ tool/install_chrome.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DEB_FILENAME=google-chrome-stable_current_amd64.deb
+SAVE_FILENAME=google-chrome.deb
+sudo apt install -yq \
+    wget \
+    fonts-liberation \
+    libnss3 \
+    libnspr4 \
+    libu2f-udev \
+    xdg-utils
+
+wget --quiet https://dl.google.com/linux/direct/${DEB_FILENAME} -O ${SAVE_FILENAME}
+dpkg -i ${SAVE_FILENAME}
+rm -rf ${SAVE_FILENAME}
