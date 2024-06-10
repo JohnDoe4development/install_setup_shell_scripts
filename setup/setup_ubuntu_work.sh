e183ff097ae090d38fdd3ecd05cf04d06a892fc9
JohnDoe <John.Doe4development.posethoye@gmail.com>
2024-06-10 16:09:36 +0900

    feat: bashrc,bash_aliasesの編集,  作業ディレクトリ作成処理の追加

---
+++ setup/setup_ubuntu_work.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# init
+sudo apt update && sudo apt upgrade
+
+# work
+mkdir -p ~/work
+mkdir -p ~/work/dev
+mkdir -p ~/work/dev/github_pj
+mkdir -p ~/work/dev/other_pj
+mkdir -p ~/bin
+mkdir -p ~/.local/bin
+mkdir ~/Downloads
+
+# ~/.bashrc
+echo 'export PATH=$PATH:~/bin:~/.local/bin' >> ~/.bashrc
+sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
+# ~/.bash_aliases
+echo "alias ls='ls -la'" >> ~/.bash_aliases
+
+# set title for windows terminal
+tee -a ~/.bashrc << "EOF" > /dev/null
+settitle () {
+  # export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+  echo -ne '\033]0;'"$1"'\a'
+}
+settitle ${USER}@${NAME}
+
+EOF
+
+# ホームディレクトリの日本語フォルダを強制的に英語名フォルダに変更
+sudo -u ${USER} LANG=C xdg-user-dirs-update --force
