2e6cff1e6cd5fc2f3b2d777669c624938ef9974e
JohnDoe <John.Doe4development.posethoye@gmail.com>
2024-06-22 10:36:50 +0900

    fix: インストール後に不要ファイルが残ってしまう不具合の修正

--- tool/install_lazygit.sh
+++ tool/install_lazygit.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/Downloads
-cd ~/Downloads
+mkdir -p ~/Downloads/tmp_lazygit
+pushd ~/Downloads/tmp_lazygit
 
 # lazygit
 wget -O lazygit.tar.gz https://github.com/jesseduffield/lazygit/releases/download/v0.41.0/lazygit_0.41.0_Linux_x86_64.tar.gz
 tar -xzf lazygit.tar.gz
 chmod +x lazygit
 mv ./lazygit ~/bin/
+
+popd
+rm -rf ~/Downloads/tmp_lazygit
 
 mkdir -p ~/.config/lazygit
 tee ~/.config/lazygit/config.yml << EOF > /dev/null
