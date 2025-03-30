0c9bbbb98dbb1a08b6c4d4a1aaf27125263fc2fc
JohnDoe <John.Doe4development.posethoye@gmail.com>
2025-03-30 15:40:59 +0900

    fix: 誤記の修正

--- setup/install_zoxide.sh
+++ setup/install_zoxide.sh
@@ -5,6 +5,6 @@ wget --quiet -O ./zoxide.tar.gz https://github.com/ajeetdsouza/zoxide/releases/d
 mkdir ./zoxide && tar -xzf zoxide.tar.gz -C ./zoxide
 mv ./zoxide ~/bin/
 echo 'export PATH=${PATH}:~/bin/zoxide' >> ~/.bashrc
-echo "eval \"$(zoxide init bash)\"" >> ~/.bashrc
+echo 'eval "$(zoxide init bash)"' >> ~/.bashrc
 
 rm -rf ./zoxide.tar.gz
