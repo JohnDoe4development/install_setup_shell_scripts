44b033066fc6f0abe2f90361e723a02668a07042
JohnDoe <John.Doe4development.posethoye@gmail.com>
2025-04-09 17:21:39 +0900

    fix: 誤記の修正

--- setup/setup_fzf_etc.sh
+++ setup/setup_fzf_etc.sh
@@ -12,7 +12,7 @@ echo 'source /usr/share/doc/fzf/examples/key-bindings.bash' >> ~/.bashrc
 tee -a ~/.bashrc << EOF > /dev/null
 export FZF_DEFAULT_OPTS="--ansi -e --prompt='QUERY> ' --layout=reverse --border=rounded --height 100%"
 export FZF_CTRL_T_OPTS="--preview 'bat --color=always --style=header,grid --line-range :100 {}'"
-export FZF_ALT_C_OPTS="--preview 'exa {} -h -T -F  --no-user --no-time --no-filesize --no-permissions --long | head -200'"
+export FZF_ALT_C_OPTS="--preview 'eza {} -h -T -F  --no-user --no-time --no-filesize --no-permissions --long | head -200'"
 export FZF_DEFAULT_COMMAND="fd -H -E .git --color=always"
 export FZF_CTRL_T_COMMAND="fd --type f -H -E .git"
 export FZF_ALT_C_COMMAND="fd --type d -H -E .git"
